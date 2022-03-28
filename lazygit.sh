f286a3b53dfc41bbc31fb672c190288c53888117
Francesco Barbieri <fbarbieri.business@gmail.com>
2022-03-29 00:26:22 +0200

    Changes made on Tue Mar 29 00:26:22 CEST 2022

--- lazygit.sh
+++ lazygit.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [ $1 = "-p" ]
-then
-    if [ $2 = "-m" ] && [ ! -z "$3" ]
-    then
-        git add .
-        git commit -m "$3"
-        git push
-    else
-        date=$(date)
-        git add .
-        git commit -m "Changes made on $date"
-        git push
-    fi
-else
-    echo "Error"
-fi
+currentBranch=$(git symbolic-ref --short -q HEAD)
+
+case "$1" in
+    "-p")
+        if [ "$2" = "-m" ] && [ ! -z "$3" ]
+        then
+            git add .
+            git commit -m "$3"
+            git push origin $currentBranch
+        else
+            date=$(date)
+            git add .
+            git commit -m "Changes made on $date"
+            git push origin $currentBranch
+        fi
+    ;;
+    
+    *)
+        echo "Error, type -h for help"
+    ;;
+esac
