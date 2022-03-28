60a5c6a32b524a329bbcf6c2b48d92b2a1090bde
Francesco Barbieri <fbarbieri.business@gmail.com>
2022-03-28 17:26:30 +0200

    Changes made on Mon Mar 28 17:26:30 CEST 2022

--- lazygit.sh
+++ lazygit.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 if [ $1 = "-p" ]
+then
+    if [ $2 = "-m" ] && [ -z "$3" ]
     then
-    git add .
-    git commit -m "test2"
-    git push
+        git add .
+        git commit -m "$3"
+        git push
+    else
+        date=$(date)
+        git add .
+        git commit -m "Changes made on $date"
+        git push
+    fi
+else
+    echo "Error"
 fi
